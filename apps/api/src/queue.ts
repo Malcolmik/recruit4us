cbe8bec188ad5b31b115776b820c6371671117ea
Malcolmik <163319850+Malcolmik@users.noreply.github.com>
2025-08-27 20:02:03 +0100

    ci: fix typecheck - add @types, web tsconfig, bullmq v5 connection options

--- apps/api/src/queue.ts
+++ apps/api/src/queue.ts
@@ -1,15 +1,21 @@
-import { Queue, Worker } from 'bullmq';
-import IORedis from 'ioredis';
+import { Queue, Worker, type ConnectionOptions } from 'bullmq';
 import { env } from './env.js';
 import { sendText } from './whatsapp.js';
 import { twilioSendText } from './twilio.js';
 
-const useTwilio = !!(env.TWILIO_ACCOUNT_SID && env.TWILIO_AUTH_TOKEN && env.TWILIO_WHATSAPP_FROM);
-const url = env.REDIS_URL || '';
-const enableRedis = /^rediss?:\/\//.test(url) && !/dummy/i.test(url);
+const useTwilio =
+  !!(env.TWILIO_ACCOUNT_SID && env.TWILIO_AUTH_TOKEN && env.TWILIO_WHATSAPP_FROM);
 
-const connection = enableRedis
-  ? new IORedis(url, { maxRetriesPerRequest: null, enableReadyCheck: false })
+const url = env.REDIS_URL || '';
+const enableRedis = /^rediss?:\/\//i.test(url) && !/dummy/i.test(url);
+
+// BullMQ v5: pass connection **options** object
+const connection: ConnectionOptions | undefined = enableRedis
+  ? {
+      url,
+      maxRetriesPerRequest: null,
+      enableReadyCheck: false,
+    }
   : undefined;
 
 export const remindersQueue: any = enableRedis
